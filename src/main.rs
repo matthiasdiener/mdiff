b8942ac4f6d7b60cb192864e2746f94586686571
Matthias Diener <mdiener@illinois.edu>
2019-07-07 17:18:04 -0500

    functional version

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use difference::{Difference, Changeset};
-use std::io::Read;
-use std::env::args;
-use gtk::prelude::*;
-use gio::prelude::*;
-use std::fs::File;
-use gtk::Builder;
-use std::io::BufReader;
+use difference::{Changeset, Difference};
+use sourceview::ViewExt;
+use std::convert::TryInto;
 
-// use gtk::{Application, ApplicationWindow, TextView};
+use gio::prelude::*;
+use gtk::prelude::*;
+use gtk::Builder;
+use std::env::args;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
 
 // upgrade weak reference or return
 #[macro_export]
@@ -54,13 +55,26 @@ pub fn build_ui(application: &gtk::Application) {
         .expect("Couldn't get builder");
     let open_button2: gtk::ToolButton = builder
         .get_object("open_button2")
-        .expect("Couldn't get builder");    
-    let text_view1: gtk::TextView = builder
-        .get_object("text_view1")
-        .expect("Couldn't get text_view");
-    let text_view2: gtk::TextView = builder
-        .get_object("text_view2")
-        .expect("Couldn't get text_view");
+        .expect("Couldn't get builder");
+    let scrolled_window1: gtk::ScrolledWindow = builder
+        .get_object("scrolled_window1")
+        .expect("Couldn't get builder");
+    let scrolled_window2: gtk::ScrolledWindow = builder
+        .get_object("scrolled_window2")
+        .expect("Couldn't get builder");
+
+    let text_view1 = sourceview::View::new();
+    let text_view2 = sourceview::View::new();
+
+    scrolled_window1.add(&text_view1);
+    scrolled_window2.add(&text_view2);
+
+    text_view1.set_property_monospace(true);
+    text_view1.set_draw_spaces(sourceview::DrawSpacesFlags::ALL);
+    text_view1.set_wrap_mode(gtk::WrapMode::WordChar);
+    text_view2.set_property_monospace(true);
+    text_view2.set_wrap_mode(gtk::WrapMode::WordChar);
+    text_view2.set_draw_spaces(sourceview::DrawSpacesFlags::ALL);
 
     let window_weak = window.downgrade();
     open_button1.connect_clicked(clone!(text_view1 => move |_| {
@@ -120,78 +134,140 @@ pub fn build_ui(application: &gtk::Application) {
                 .get_buffer()
                 .expect("Couldn't get window")
                 .set_text(&contents);
-            // text_view2.set_wrap_mode(gtk::WrapMode::Word);
         }
         file_chooser.destroy();
     }));
 
-    text_view1.get_buffer().unwrap().connect_changed(clone!(text_view1 => move |_| {
-        // let t1 = text_view1.clone();
-        // let t2 = text_view2.clone();
-        diff(&text_view1, &text_view2);
-    }));
+    text_view1
+        .get_buffer()
+        .unwrap()
+        .connect_changed(clone!(text_view1, text_view2 => move |_| {
+            diff(&text_view1.get_buffer().unwrap(), &text_view2.get_buffer().unwrap());
+        }));
 
-    // text_view2.get_buffer().unwrap().connect_changed(move |_| {
-    //     let t1 = text_view1.clone();
-    //     let t2 = text_view2.clone();
-    //     diff(&t1, &t2);
-    // });
-    
+    text_view2
+        .get_buffer()
+        .unwrap()
+        .connect_changed(clone!(text_view1, text_view2 => move |_| {
+            diff(&text_view1.get_buffer().unwrap(), &text_view2.get_buffer().unwrap());
+        }));
 
+    let added_tag1 = gtk::TextTag::new(Some("added"));
+    added_tag1.set_property_background(Some("green"));
+
+    let removed_tag1 = gtk::TextTag::new(Some("removed"));
+    removed_tag1.set_property_background(Some("red"));
+
+    let equal_tag1 = gtk::TextTag::new(Some("equal"));
+    equal_tag1.set_property_background(Some("white"));
+
+    let added_tag2 = gtk::TextTag::new(Some("added"));
+    added_tag2.set_property_background(Some("green"));
+
+    let removed_tag2 = gtk::TextTag::new(Some("removed"));
+    removed_tag2.set_property_background(Some("red"));
+
+    let equal_tag2 = gtk::TextTag::new(Some("equal"));
+    equal_tag2.set_property_background(Some("white"));
+
+    text_view1
+        .get_buffer()
+        .unwrap()
+        .get_tag_table()
+        .unwrap()
+        .add(&added_tag1);
+    text_view1
+        .get_buffer()
+        .unwrap()
+        .get_tag_table()
+        .unwrap()
+        .add(&removed_tag1);
+    text_view1
+        .get_buffer()
+        .unwrap()
+        .get_tag_table()
+        .unwrap()
+        .add(&equal_tag1);
+
+    text_view2
+        .get_buffer()
+        .unwrap()
+        .get_tag_table()
+        .unwrap()
+        .add(&added_tag2);
+    text_view2
+        .get_buffer()
+        .unwrap()
+        .get_tag_table()
+        .unwrap()
+        .add(&removed_tag2);
+    text_view2
+        .get_buffer()
+        .unwrap()
+        .get_tag_table()
+        .unwrap()
+        .add(&equal_tag2);
 
     window.show_all();
 }
 
+fn diff(b1: &gtk::TextBuffer, b2: &gtk::TextBuffer) {
+    b1.remove_all_tags(&b1.get_start_iter(), &b1.get_end_iter());
+    b2.remove_all_tags(&b2.get_start_iter(), &b2.get_end_iter());
 
-fn diff(t1: &gtk::TextView, t2: &gtk::TextView) {
-    let b1 = t1.get_buffer().unwrap();
-    let b2 = t2.get_buffer().unwrap();
-    let text1 = b1.get_text(&b1.get_start_iter(), &b1.get_end_iter(), false).unwrap();
-    let text2 = b2.get_text(&b2.get_start_iter(), &b2.get_end_iter(), false).unwrap();
-
-    // let mut t = term::stdout().unwrap();
-
-    // println!("{:?}", text1);
+    let text1 = b1
+        .get_slice(&b1.get_start_iter(), &b1.get_end_iter(), true)
+        .unwrap();
+    let text2 = b2
+        .get_slice(&b2.get_start_iter(), &b2.get_end_iter(), true)
+        .unwrap();
 
     let Changeset { diffs, .. } = Changeset::new(&text1, &text2, "");
 
+    let mut iter1 = b1.get_start_iter();
+
     for c in &diffs {
         match *c {
             Difference::Same(ref z) => {
-                // t.fg(term::color::RED).unwrap();
-                print!("={}", z);
+                let len = z.chars().count();
+                let tmpiter = b1.get_iter_at_offset(iter1.get_offset());
+                iter1.forward_chars(len.try_into().unwrap());
+                b1.apply_tag_by_name("equal", &tmpiter, &iter1);
+                // println!("={} ({}-{}, {})", z, tmpiter.get_offset(), iter1.get_offset(), len);
             }
             Difference::Rem(ref z) => {
-                // t.fg(term::color::WHITE).unwrap();
-                // t.bg(term::color::RED).unwrap();
-                print!("-{}", z);
-                // t.reset().unwrap();
+                let len = z.chars().count();
+                let tmpiter = b1.get_iter_at_offset(iter1.get_offset());
+                iter1.forward_chars(len.try_into().unwrap());
+                b1.apply_tag_by_name("removed", &tmpiter, &iter1);
+                // println!("-{} ({}-{}, {})", z, tmpiter.get_offset(), iter1.get_offset(), len);
             }
             _ => (),
         }
     }
-    // t.reset().unwrap();
+    // println!("************************");
 
-    // writeln!(t, "");
-
+    let mut iter2 = b2.get_start_iter();
     for c in &diffs {
         match *c {
             Difference::Same(ref z) => {
-                // t.fg(term::color::GREEN).unwrap();
-                print!("={}", z);
+                let len = z.chars().count();
+                let tmpiter = b2.get_iter_at_offset(iter2.get_offset());
+                iter2.forward_chars(len.try_into().unwrap());
+                b2.apply_tag_by_name("equal", &tmpiter, &iter2);
+                // println!("={} ({}-{}, {})", z, tmpiter.get_offset(), iter2.get_offset(), len);
             }
             Difference::Add(ref z) => {
-                // t.fg(term::color::WHITE).unwrap();
-                // t.bg(term::color::GREEN).unwrap();
-                print!("+{}", z);
-                // t.reset().unwrap();
+                let len = z.chars().count();
+                let tmpiter = b2.get_iter_at_offset(iter2.get_offset());
+                iter2.forward_chars(len.try_into().unwrap());
+                b2.apply_tag_by_name("added", &tmpiter, &iter2);
+                // println!("+{} ({}-{}, {})", z, tmpiter.get_offset(), iter2.get_offset(), len);
             }
             _ => (),
         }
     }
-    println!("");
-    // t.reset().unwrap();
-    // t.flush().unwrap();
+    // println!("$$$$$$$$$$$$$$$$$$$$$$$");
 }
 
 fn main() {
